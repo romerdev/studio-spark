cf2c0016f66b91e8951443bab63cba4c64cc60b6
Romer Bormann <55235748+RomerBormann@users.noreply.github.com>
2020-09-14 19:44:21 +0200

    Begin met maken van homepage

--- private/views/homepage.php
+++ private/views/homepage.php
@@ -4,10 +4,32 @@
 
 <?php $this->layout('website');?>
 
-<h1>Homepage</h1>
+<div class="masthead">
+    <div class="container h-100">
+        <div class="row h-100 align-items-center">
+            <div class="col-12 text-center">
+                <h1 class="font-weight-light">Wij hebben een creatieve oplossing</h1>
+                <p class="lead">Nu nog het probleem..</p>
+            </div>
+        </div>
+    </div>
+</div>
 
-<p>Hier komt alle content voor de homepage.</p>
+<section class="container">
+    <div class="row">
+        <div class="col-12 text-center">
+            <h1>Let's make creative spark!</h1>
+        </div>
 
-
-
-
+    </div>
+        <div class="row">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="https://images.unsplash.com/photo-1496167117681-944f702be1f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+</section>
