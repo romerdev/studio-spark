6f70b3368f4b7ea505db8f5272f4925b1de34a62
Romer Bormann <55235748+RomerBormann@users.noreply.github.com>
2020-09-14 18:14:44 +0200

    Final fixed footer

--- private/views/website.php
+++ private/views/website.php
@@ -28,31 +28,21 @@
         </section>
     </main>
 
-    <!-- Footer -->
     <footer class="page-footer font-small mdb-color pt-4">
 
-        <!-- Footer Links -->
         <div class="container text-center text-md-left">
 
-            <!-- Footer links -->
             <div class="row text-center text-md-left mt-3 pb-3">
 
-                <!-- Grid column -->
                 <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
                     <h6 class="text-uppercase mb-4 font-weight-bold">Spark</h6>
                     <p>Wij denken graag met jou mee voor een creatieve oplossing op jouw media-probleem. Van visitekaartjes tot een volledige website. Neem snel contact met ons op voor een offerte.</p>
                 </div>
-                <!-- Grid column -->
 
                 <hr class="w-100 clearfix d-md-none">
 
-                <!-- Grid column -->
-
-                <hr class="w-100 clearfix d-md-none">
-
-                <!-- Grid column -->
                 <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
-                    <h6 class="text-uppercase mb-4 font-weight-bold">Useful links</h6>
+                    <h6 class="text-uppercase mb-4 font-weight-bold">Navigatie</h6>
                     <p>
                         <a href="<?php echo url( 'home' ) ?>">Home</a>
                     </p>
@@ -72,7 +62,7 @@
 
                 <hr class="w-100 clearfix d-md-none">
 
-                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                <div class="col-md-4 col-lg-4 col-xl-4 mx-auto mt-4">
                     <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
                     <p>
                         <i class="fas fa-home mr-3"></i> Contactweg 36, 1014 AN, Amsterdam</p>
@@ -86,13 +76,10 @@
 
             <hr>
 
-            <!-- Grid row -->
             <div class="row d-flex align-items-center">
 
-                <!-- Grid column -->
                 <div class="col-md-7 col-lg-8">
 
-                    <!--Copyright-->
                     <p class="text-center text-md-left">© 2020 Copyright:
                         <a href="#">
                             <strong> spark.nl</strong>
@@ -100,12 +87,9 @@
                     </p>
 
                 </div>
-                <!-- Grid column -->
 
-                <!-- Grid column -->
                 <div class="col-md-5 col-lg-4 ml-lg-0">
 
-                    <!-- Social buttons -->
                     <div class="text-center text-md-right">
                         <ul class="list-unstyled list-inline">
                             <li class="list-inline-item">
@@ -132,17 +116,12 @@
                     </div>
 
                 </div>
-                <!-- Grid column -->
 
             </div>
-            <!-- Grid row -->
 
         </div>
-        <!-- Footer Links -->
 
     </footer>
-    <!-- Footer -->
-
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
