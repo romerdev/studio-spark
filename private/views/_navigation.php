dafc7eca21b6906d7bff5c9ca467358f71616490
Romer Bormann <55235748+RomerBormann@users.noreply.github.com>
2020-09-15 17:28:21 +0200

    Kaartjes toegevoegd en menu-kleur aangepast

--- private/views/_navigation.php
+++ private/views/_navigation.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark">
     <a class="navbar-brand" href="#">SPARK</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="white-color"><i class="fas fa-bars" style="font-size:24px;"></i></span>
+        <span class="white-color"><i class="fas fa-bars" style="font-size:24px; color: white;"></i></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarText">
         <ul class="navbar-nav ml-auto">
--- private/views/homepage.php
+++ private/views/homepage.php
@@ -15,21 +15,43 @@
     </div>
 </div>
 
-<section class="container">
-    <div class="row">
+<section class="container pt-5 pb-3">
+    <div class="row pb-5">
         <div class="col-12 text-center">
             <h1>Let's make creative spark!</h1>
         </div>
-
     </div>
-        <div class="row">
-            <div class="card" style="width: 18rem;">
+
+    <div class="row py-2">
+        <div class="col-sm-4 pb-3">
+            <div class="card">
                 <img class="card-img-top" src="https://images.unsplash.com/photo-1496167117681-944f702be1f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80" alt="Card image cap">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <h5 class="card-title">Uit Database</h5>
+                    <p class="card-text">Hier komt één van de 3 onderdelen vanuit onze portfolio. Met eem mooi plaatje, pakkende titel en een knop naar dat onderdeel.</p>
+                    <a href="#" class="btn btn-primary">Naar het onderdeel</a>
                 </div>
             </div>
         </div>
+        <div class="col-sm-4 pb-3">
+            <div class="card">
+                <img class="card-img-top" src="https://images.unsplash.com/photo-1496167117681-944f702be1f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Uit Database</h5>
+                    <p class="card-text">Hier komt één van de 3 onderdelen vanuit onze portfolio. Met eem mooi plaatje, pakkende titel en een knop naar dat onderdeel.</p>
+                    <a href="#" class="btn btn-primary">Naar het onderdeel</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4 pb-3">
+            <div class="card">
+                <img class="card-img-top" src="https://images.unsplash.com/photo-1496167117681-944f702be1f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Uit Database</h5>
+                    <p class="card-text">Hier komt één van de 3 onderdelen vanuit onze portfolio. Met eem mooi plaatje, pakkende titel en een knop naar dat onderdeel.</p>
+                    <a href="#" class="btn btn-primary">Naar het onderdeel</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
