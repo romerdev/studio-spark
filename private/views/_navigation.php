6e0501de23433ecdca8554c6a48361035db74990
Romer Bormann <55235748+RomerBormann@users.noreply.github.com>
2020-09-16 15:12:28 +0200

    Active Nav kleur

--- private/views/_navigation.php
+++ private/views/_navigation.php
@@ -6,19 +6,19 @@
     <div class="collapse navbar-collapse" id="navbarText">
         <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="<?php echo url( 'home' ) ?>"<?php if ( current_route_is( 'home' ) ): ?> class="active"<?php endif ?>>Home</a>
+                    <a class="nav-link" href="<?php echo url( 'home' ) ?>"<?php if ( current_route_is( 'home' ) ): ?> id="active"<?php endif ?>>Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="<?php echo url( 'about' ) ?>"<?php if ( current_route_is( 'about' ) ): ?> class="active"<?php endif ?>>Over Ons</a>
+                    <a class="nav-link" href="<?php echo url( 'about' ) ?>"<?php if ( current_route_is( 'about' ) ): ?> id="active"<?php endif ?>>Over Ons</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="<?php echo url( 'diensten' ) ?>"<?php if ( current_route_is( 'diensten' ) ): ?> class="active"<?php endif ?>>Diensten</a>
+                    <a class="nav-link" href="<?php echo url( 'diensten' ) ?>"<?php if ( current_route_is( 'diensten' ) ): ?> id="active"<?php endif ?>>Diensten</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="<?php echo url( 'portfolio' ) ?>"<?php if ( current_route_is( 'portfolio' ) ): ?> class="active"<?php endif ?>>Portfolio</a>
+                    <a class="nav-link" href="<?php echo url( 'portfolio' ) ?>"<?php if ( current_route_is( 'portfolio' ) ): ?> id="active"<?php endif ?>>Portfolio</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="<?php echo url( 'contact' ) ?>"<?php if ( current_route_is( 'contact' ) ): ?> class="active"<?php endif ?>>Contact</a>
+                    <a class="nav-link" href="<?php echo url( 'contact' ) ?>"<?php if ( current_route_is( 'contact' ) ): ?> id="active"<?php endif ?>>Contact</a>
                 </li>
             </ul>
         </div>
